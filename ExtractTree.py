fc72445b673f641433d576116dbd52556433ef7d
Alex Coleman <LordMailman@users.noreply.github.com>
2018-06-29 14:17:26 -0400

    Updated to work with multiple arguments and to time total execution. Updated requirements and added .vscode tabSize setting

--- ExtractTree.py
+++ ExtractTree.py
@@ -1,18 +1,20 @@
 #!python
 """Python code to search through directories and find specific files"""
 
-import os
 import argparse
-import warnings
+import os
 from pyunpack import Archive, PatoolError
+import time
 import ipdb
+
+# Get starting time
+START_TIME = time.time()
 
 def extract(root, filename):
     """Extract the archive to it's containing folder"""
     try:
         Archive(os.path.join(root, filename)).extractall(root)
-    except PatoolError as err:
-        # warnings.warn(err.args)
+    except PatoolError:
         print('Failed to extract ' + os.path.join(root, filename))
 
 # Define extractable extensions
@@ -21,18 +23,27 @@ VALID_EXT = ('.zip','.rar','.7z')
 # import argparse to get target root dir
 PARSER = argparse.ArgumentParser()
 PARSER.add_argument('path',
+                    nargs='+',
                     help='Path to recursively extract from')
 ARGS = PARSER.parse_args()
 
-# Check path input
-if os.path.isdir(ARGS.path) is not True:
-    raise Exception('Path input to DirSearch must be a valid directory')
+# Loop over every input path
+NUM_EXTRACTED = 0
+for path in ARGS.path:
+    # Check path input
+    if os.path.isdir(path) is not True:
+        print('{} is not a valid directory.'.format(path))
+        continue
 
-# Loop through all files
-for root, directories, filenames in os.walk(ARGS.path):
-    # ipdb.set_trace()
-    for filename in filenames:
-        if filename.endswith(VALID_EXT):
-            print('Extracting ' + os.path.join(root, filename))
-            extract(root, filename)
+    # Loop through all files
+    for root, directories, filenames in os.walk(path):
+        # ipdb.set_trace()
+        for filename in filenames:
+            if filename.endswith(VALID_EXT):
+                print('Extracting ' + os.path.join(root, filename))
+                extract(root, filename)
+                NUM_EXTRACTED += 1
 
+# Print end time
+TOTAL_TIME = time.time() - START_TIME
+print('Extracted {0} files in {1} seconds'.format(NUM_EXTRACTED, TOTAL_TIME))
